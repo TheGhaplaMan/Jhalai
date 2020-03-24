66827ad0c3cd89f821972bfbfc75fb06345593bb
TheGhaplaMan <apagla988@gmail.com>
2020-03-25 00:51:16 +0600

    ei lo haua

---
+++ nigga.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a;
+  cin >> a;
+  if (a<3)
+  cout << "rasel vodai";
+  else if (a<9)
+  cout << "sagor vodai";
+  else
+  cout << "thikasse, tanshed vodai";
+  return 0;
+}
